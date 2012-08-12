ec9d00b4d14a3e856639eff1282f1fe4104b4f41
Stephen Bartlett <stephenb@rtlett.org>
2012-08-12 22:55:30 +1000

    stack for jenkins

--- lib/dupondius/aws/stacks.rb
+++ lib/dupondius/aws/stacks.rb
@@ -1,4 +1,6 @@
+require 'dupondius/aws/stacks/base'
 require 'dupondius/aws/stacks/dashboard'
+require 'dupondius/aws/stacks/continuous_integration'
 require 'dupondius/aws/stacks/rails'
 
 module Dupondius; module Aws; module Stacks
@@ -6,6 +8,46 @@ module Dupondius; module Aws; module Stacks
   def self.cloudformation
     @cfn ||= AWS::CloudFormation.new(:access_key_id => Dupondius::Aws::Config.access_key,
        :secret_access_key => Dupondius::Aws::Config.secret_access_key)
+  end
+
+  class Base
+
+    def initialize project
+      @project = project
+    end
+
+    def self.template name
+       self.class_eval("def template_name; '#{name}';end")
+    end
+
+    def create parameters
+      Dupondius::Aws::Stacks.cloudformation.stacks.create("#{template_name}#{@project}", as_json,
+        :parameters => parameters)
+    end
+
+    def params
+      @param ||= validate[:parameters].collect { |p| p[:parameter_key] }
+    end
+
+    def validate
+      Dupondius::Aws::Stacks.cloudformation.validate_template(as_json)
+    end
+
+    def as_json
+      @template_json ||= File.open(File.expand_path(File.join(
+        File.dirname(__FILE__), '..', 'templates', "#{template_name}.template")), 'rb').read
+    end
+  end
+
+  class ContinuousIntegration < Base
+
+    template :jenkins
+  end
+
+
+  class Dashboard < Base
+
+    template :dashboard
   end
 
 end; end; end
--- lib/dupondius/aws/tasks/aws.rake
+++ lib/dupondius/aws/tasks/aws.rake
@@ -16,6 +16,19 @@ namespace :dupondius do
       result = dashboard.create(specified_params)
       puts result
     end
+
+    desc 'Creates a new continuous integration instance from the given project'
+    task :create_ci, [:project_name ] do |t, args|
+
+      instance = Dupondius::Aws::Stacks::ContinuousIntegration.new args.project_name
+
+      specified_params = ENV.to_hash.select { |k,v| instance.params.include? k }
+      if instance.params.size != specified_params.size
+        raise "Unable to find required params in ENV. #{instance.params}"
+      end
+      result = instance.create(specified_params)
+      puts result
+    end
   end
 
 end

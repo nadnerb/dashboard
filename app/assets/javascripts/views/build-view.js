2900264d93e83fb76b50184f96d8d4522bdc14af
azzamallow <azzamallow@gmail.com>
2012-08-25 15:33:38 +1000

    popovers now rerender properly, can now see if a build is already in queue

--- app/assets/javascripts/views/build-view.js
+++ app/assets/javascripts/views/build-view.js
@@ -15,7 +15,47 @@ define([
             'click .build-now': 'buildNow'
         },
 
+        renderSpinner: function () {
+            if (this.model.get('color').indexOf('anim') !== -1) {
+                if (this.$('.spinner svg').length === 0) {
+                    spinner('spinner-' + this.model.get('displayName'), 16, 15, 15, 3, '#fff');
+                }
+            } else {
+                this.$('.spinner').empty();
+            }
+        },
+
         postRender: function () {
+            this.renderQueued();
+        },
+
+        renderQueued: function () {
+            if (this.model.get('inQueue') === true) {
+                if (this.$('.build-now .queued').length === 0) {
+                    this.$('.build-now i').hide();
+                    this.$('.build-now').append(this.make('span', {class: 'queued'}, 'Queued'));
+                }
+            } else {
+                this.$('.build-now i').show();
+                this.$('.build-now .queued').remove();
+            }
+        },
+
+        update: function (model) {
+            this.model = model;
+            if (this.$('.build-' + this.model.get('color')).length === 0) {
+                this.$('.build-button')
+                    .removeClass('build-blue')
+                    .removeClass('build-blue_anime')
+                    .removeClass('build-red')
+                    .removeClass('build-red_anime')
+                    .removeClass('build-grey')
+                    .removeClass('build-grey_anime')
+                    .addClass('build-' + model.get('color'));
+            }
+
+            this.renderQueued();
+
             if (this.model.isCurrentlyFailing()) {
                 var lastFailed = new LastFailed({displayName: this.model.get('displayName')});
 
@@ -36,36 +76,18 @@ define([
                 }, this);
 
                 lastFailed.fetch();
-            }
-        },
-
-        renderSpinner: function () {
-            if (this.model.get('color').indexOf('anim') !== -1) {
-                if (this.$('.spinner svg').length === 0) {
-                    spinner('spinner-' + this.model.get('displayName'), 16, 15, 15, 3, '#fff');
-                }
             } else {
-                this.$('.spinner').empty();
-            }
-        },
-
-        update: function (model) {
-            this.model = model;
-            if (this.$('.build-' + this.model.get('color')).length === 0) {
-                this.$('.build-button')
-                    .removeClass('build-blue')
-                    .removeClass('build-blue_anime')
-                    .removeClass('build-red')
-                    .removeClass('build-red_anime')
-                    .removeClass('build-grey')
-                    .removeClass('build-grey_anime')
-                    .addClass('build-' + model.get('color'));
+                this.$('.build-' + this.model.get('color') + ' a').removeAttr('data-title').removeAttr('data-content');
             }
 
             this.renderSpinner();
         },
 
         buildNow: function (event) {
+            if (this.model.get('inQueue') === true) {
+                return false;
+            }
+
             var BuildNow = Backbone.Model.extend({
                 url: $(event.currentTarget).attr('href'),
                 sync: function(method, model, options) {  

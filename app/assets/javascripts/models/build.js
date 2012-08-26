029d13adf2b36bc7c01e7992d99ee130b55ba59d
azzamallow <azzamallow@gmail.com>
2012-08-26 13:36:03 +1000

    renamed a build

--- app/assets/javascripts/models/build.js
+++ app/assets/javascripts/models/build.js
@@ -5,7 +5,7 @@ define([
     var NAMES = {
         'Build_Project': 'Build',
         'Acceptance_Test': 'Acceptance',
-        'Deployment_Test': 'Deploy',
+        'Deployment_Test': 'Deployment Test',
         'Deploy_To_QA': 'Deploy to QA',
         'Deploy_To_Staging': 'Deploy to Staging',
         'Deploy_To_Production': 'Deploy to Production'

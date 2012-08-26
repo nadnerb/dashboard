14fb0b6ec24ce9836abc0b63247ab0e51fcdefd0
Ronald Holshausen <uglyog@gmail.com>
2012-08-26 15:41:09 +1000

    trying to get cap to deploy the pipeline tag

--- config/deploy/canary.rb
+++ config/deploy/canary.rb
@@ -1,6 +1,7 @@
 set :domain,      "canary.dupondi.us"
 set :rails_env,   "canary"
 set :app_env,     "canary"
+set :branch,      ENV["PIPELINE_VERSION"] || 'master'
 
 role :web, domain
 role :app, domain

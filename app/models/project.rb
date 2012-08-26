0c0b035e7e242cbd788f911267335bb999592cd3
Stephen Bartlett <stephenb@rtlett.org>
2012-08-26 11:24:14 +1000

    Dashboard installed at EC2 instance creation time.
    When the dashboard template is run, it will install the dashboard
    rails app in a similar manner to how Cap will deploy it.
    
    This could be revised into a RPM package or similar.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,14 +3,19 @@ class Project < ActiveRecord::Base
 
   validates_presence_of :name
 
-  #after_create :launch_dashboard
+  after_create :launch_dashboard
 
   def launch_dashboard
-    Dupondius::Aws::Stacks::Dashboard.create(self.name, {KeyName: 'team-brats',
-                                                         InstanceType: 't1.micro'})
+    # temporarily guard the creation of aws resources
+    if ENV['AWS_ENABLED'] == 'true'
+      Dupondius::Aws::Stacks::Dashboard.create(self.name, {KeyName: 'team-brats',
+                                                           InstanceType: 't1.micro',
+                                                           AwsAccessKey: Dupondius::Aws::Config.access_key,
+                                                           AwsSecretAccessKey: Dupondius::Aws::Config.secret_access_key })
+    end
   end
 
-  #handle_asynchronously :launch_dashboard
+  handle_asynchronously :launch_dashboard
 
   def dashboard
     @dashboard ||= Dupondius::Aws::Stacks::Dashboard.find(self.name)

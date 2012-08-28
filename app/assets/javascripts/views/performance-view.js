a6b49010b9633c4febdf898aa4625a4826104859
Brendan Spinks <brendanspinks@gmail.com>
2012-08-28 19:34:59 +1000

    temporary new relic graph from active admin

--- app/assets/javascripts/views/performance-view.js
+++ app/assets/javascripts/views/performance-view.js
@@ -1,5 +1,12 @@
 define([
-    'vendor/base'
-], function (BackboneSuperView) {
-    return BackboneSuperView.extend({});
+    'vendor/base',
+    'text!templates/performance.html.haml'
+], function (BackboneSuperView, template) {
+    return BackboneSuperView.extend({
+
+        id: 'performance',
+
+        template: template
+
+    });
 });

6a04a212d7e812e99b9c3c5cf1e2bfb1cb0b42d8
Ronald Holshausen <uglyog@gmail.com>
2012-09-02 15:36:51 +1000

    jenkins pipeline done for the new project

---
+++ app/models/jobs/launch_ci.rb
@@ -0,0 +1,37 @@
+require 'awesome_print'
+require 'yajl'
+
+class Jobs::LaunchCi
+
+  attr_reader :project, :params
+
+  def initialize(project, params)
+    @project = project
+    @params = params
+    ap self
+  end
+
+  def run
+    puts "---> LauchCi"
+    user = Source::Commands.new(@project.token).user['login']
+    options = {
+        KeyName: 'team-brats',
+        InstanceType: 'm1.small',
+        ProjectGithubUser: user.to_s,
+        ProjectType: tech_stack(params[:project][:tech_stack]).to_s
+    }
+    ap options
+    Dupondius::Aws::Stacks::ContinuousIntegration.create(@project.name, options)
+    puts "LauchCi <---"
+  end
+
+  def tech_stack(tech)
+    case tech
+      when 'Ruby on Rails' then 'rails'
+      else
+        'rails'
+    end
+  end
+
+  handle_asynchronously :run
+end

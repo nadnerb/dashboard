f28fd1ac139eb49a13186a46643d0f7d48e46cee
Brendan Spinks <brendanspinks@gmail.com>
2012-09-03 08:09:53 +1000

    new relic setup info

--- app/controllers/dashboard/performance_controller.rb
+++ app/controllers/dashboard/performance_controller.rb
@@ -1,14 +1,13 @@
 class Dashboard::PerformanceController < ActionController::Base
 
   respond_to :json
-  wrap_parameters :format => [:json]
 
   def show
     newrelic ? respond_with(newrelic) : respond_with({:not_configured => true })
   end
 
   def create
-    respond_with(Dashboard::NewrelicConfiguration.create!(:token => params['token']), :location => dashboard_performance_path)
+    respond_with(Dashboard::NewrelicConfiguration.create!(params['performance']), :location => dashboard_performance_path)
   end
 
   def newrelic

27b65d2a5e9e1b06ff4d586741bf336d9f8ffb89
Ronald Holshausen <uglyog@gmail.com>
2012-09-02 15:57:13 +1000

    dont throw exceptions in project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    project = Project.create!(params[:project].except(:github, :tech_stack, :support, :environments))
+    project = Project.create(params[:project].except(:github, :tech_stack, :support, :environments))
     Jobs::Skeleton.new(project.id).run if Rails.configuration.launchpad_jobs
     Jobs::LaunchCi.new(project, params).run if Rails.configuration.aws_enabled
     respond_with(project, :location => :projects)

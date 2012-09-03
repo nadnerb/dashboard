758776ed58b55fb58170dda02cbe2f1195290c9a
Stephen Bartlett <stephenb@rtlett.org>
2012-09-03 22:12:41 +1000

    Fix id constraint

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Dashboard::Application.routes.draw do
   namespace :dashboard do
     resource :stories, :only => [:create, :show]
     resource :performance, :only => [:create, :show], :controller => 'performance'
-    resources :stacks, :constraints => { :id => /.*/ }
+    resources :stacks, :constraints => { :id => /[a-zA-Z]+-[a-zA-Z]+/ }
   end
 
   match '/status' => 'status#index'

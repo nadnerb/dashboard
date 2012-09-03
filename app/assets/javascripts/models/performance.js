a938e496f04a42b1e62debfb5118b1b4fcdfc5d4
azzamallow <azzamallow@gmail.com>
2012-09-03 21:26:59 +1000

    validation on new relic config

--- app/assets/javascripts/models/performance.js
+++ app/assets/javascripts/models/performance.js
@@ -3,6 +3,24 @@ define([
 ], function (Backbone) {
 
     return Backbone.Model.extend({
-        url: 'dashboard/performance'
+        url: 'dashboard/performance',
+
+        validate: function (attrs) {
+            if (attrs.not_configured === true){
+                return;
+            }
+
+            var errors = [];
+
+            if (attrs.token === '') {
+                errors.push({name: 'token', message: 'You must provide a token'});
+            }
+
+            if (_(errors).isEmpty()) {
+                return;
+            }
+
+            return errors;
+        }
     });
 });

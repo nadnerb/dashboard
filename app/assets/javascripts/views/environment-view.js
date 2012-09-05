9232701c1339a41f4e741a8eb2ff3b34050b5f25
azzamallow <azzamallow@gmail.com>
2012-09-05 23:07:08 +1000

    starting, stopping and removing environments

--- app/assets/javascripts/views/environment-view.js
+++ app/assets/javascripts/views/environment-view.js
@@ -1,25 +1,85 @@
 define([
     'vendor/base',
+    'models/instance',
     'text!templates/environment.html.haml'
-], function (BackboneSuperView, template) {
+], function (BackboneSuperView, Instance, template) {
     return BackboneSuperView.extend({
+
+        events: {
+            'click .start': 'start',
+            'click .stop': 'stop',
+            'click .remove': 'remove'
+        },
 
         className: 'environment',
 
         template: template,
 
-        fetchInstance: function () {
-            spinner('loading-' + this.model.get('tags')['aws:cloudformation:stack-name'], 50, 45, 15, 3, '#888');
+        initialize: function () {
+            this.bindTo(this.model, 'sync', function () {
+                this.render();
+                this.fadeIn();
+            });
+        },
 
+        fetchInstance: function () {
+            spinner('loading-' + this.model.get('tags')['dupondius:environment'], 50, 45, 15, 3, '#888');
+            this.fadeIn();
+        },
+
+        fadeIn: function () {
             var view = this;
             setTimeout(function () {
-                view.renderInstance();
+                view.$('.environment-loading').fadeOut();
+                view.$('.environment-information').css('visibility', 'visible').fadeIn();
             }, Math.random() * 1000);
         },
 
-        renderInstance: function () {
-            this.$('.environment-loading').fadeOut();
-            this.$('.environment-information').css('visibility', 'visible').fadeIn();
+        fadeOut: function () {
+            this.$('.environment-loading').fadeIn();
+            this.$('.environment-information').fadeOut();
+        },
+
+        updateInstance: function () {
+            this.$('.environment-information .status').text(this.model.get('status'));
+            this.$('.environment-information .size').text(this.model.get('instance_type'));
+        },
+
+        start: function () {
+            if (this.$('.start.disabled').length !== 0) {
+                return;
+            }
+            if (confirm("Are you sure you want to start this environment?")) {
+                this.fadeOut();
+                this.model.save({
+                    status: 'start'
+                });
+            }
+            return false;
+        },
+
+        stop: function () {
+            if (this.$('.stop.disabled').length !== 0) {
+                return;
+            }
+
+            if (confirm("Are you sure you want to stop this environment?")) {
+                this.fadeOut();
+                this.model.save({
+                    status: 'stop'
+                });
+            }
+            return false;
+        },
+
+        remove: function () {
+            if (confirm("Are you sure you want to remove this environment?")) {
+                this.fadeOut();
+                this.model.save({
+                    status: 'terminate'
+                });
+            }
+            return false;
         }
     });
 })

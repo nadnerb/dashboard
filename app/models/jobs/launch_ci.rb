4f06596d384ee93c56730379f1bedb02229f7cea
Ronald Holshausen <uglyog@gmail.com>
2012-09-08 16:07:00 +1000

    bloody jenkins bloody encrypting the bloody secret keys with the bloody java defaults!!!

--- app/models/jobs/launch_ci.rb
+++ app/models/jobs/launch_ci.rb
@@ -1,8 +1,14 @@
 require 'yajl'
+require "base64"
+require 'openssl'
+require 'digest/sha2'
 
 class Jobs::LaunchCi
 
   attr_reader :project, :params
+
+  MAGIC = "::::MAGIC::::"
+  SECRET = '7b314451c3b147f401f2330d21d848a81573c5b376317ced06d1c63d4fb65778'
 
   def initialize(project, params)
     @project = project
@@ -15,9 +21,12 @@ class Jobs::LaunchCi
         KeyName: 'team-brats',
         InstanceType: 'm1.small',
         ProjectGithubUser: user.to_s,
-        ProjectType: tech_stack(params[:project][:tech_stack]).to_s
+        ProjectType: tech_stack(params[:project][:tech_stack]).to_s,
+        AccessKey: params[:project][:aws][:accessKey],
+        SecretAccessKey: encrypt(params[:project][:aws][:secretAccessKey]),
+        PrivateKey: encrypt(params[:project][:aws][:privateKey])
     }
-    Dupondius::Aws::Stacks::ContinuousIntegration.create(@project.name, options)
+    Dupondius::Aws::CloudFormation::ContinuousIntegration.create(@project.name, options)
   end
 
   def tech_stack(tech)
@@ -26,6 +35,22 @@ class Jobs::LaunchCi
       else
         'rails'
     end
+  end
+
+  def encrypt(data)
+    blocks = (data + MAGIC).scan(/.{1,16}/)
+    encrypted_data = []
+    sha256 = Digest::SHA2.new(256)
+    aes = OpenSSL::Cipher.new("AES-128-CBC")
+    key = sha256.digest(SECRET)
+    blocks.each do |block|
+      aes.reset
+      aes.encrypt
+      aes.key = key
+      encrypted_data << aes.update(block)
+      encrypted_data << aes.final if block.length < 16
+    end
+    Base64.encode64(encrypted_data.join)
   end
 
   handle_asynchronously :run

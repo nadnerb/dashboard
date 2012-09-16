1cc585d9fd844ac3846a15180cf621dfa96218e2
Brendan Spinks <brendanspinks@gmail.com>
2012-09-16 13:02:32 +1000

    removed app scrolls, not very dry at the moment

--- app/models/jobs/rails_skeleton.rb
+++ app/models/jobs/rails_skeleton.rb
@@ -8,7 +8,16 @@ class Jobs::RailsSkeleton
   end
 
   def run
+    #p "building rails environment: #{resting_place} for project: #{project.name}"
+    #p `cd #{resting_place} && scrolls new #{project.name} -s capistrano unicorn rspec git rubyracer 2>&1`
     p "building rails environment: #{resting_place} for project: #{project.name}"
-    p `cd #{resting_place} && scrolls new #{project.name} -s capistrano unicorn rspec git rubyracer 2>&1`
+    p `cd #{resting_place};\
+    curl -u 'dupondius:4nalienatemybaby' -L -o rails.tar.gz https://api.github.com/repos/nadnerb/dupondius-rails-skeleton/tarball;\
+    tar xfz rails.tar.gz;\
+    mv \`ls -d -- */\` #{project.name};\
+    cd #{project.name};\
+    git init;\
+    git add .;\
+    git commit -m 'Initial commit'`
   end
 end
--- config/initializers/dupondius.rb
+++ config/initializers/dupondius.rb
@@ -6,7 +6,6 @@ Dupondius.configure do |config|
   config.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   config.project_name = ENV['PROJECT_NAME']
 
-  # default to production keys? need to confirm with breno
   config.github_client_id = ENV['GITHUB_CLIENT_ID'] || 'bacdc0747b0175015bd9'
   config.github_secret = ENV['GITHUB_SECRET'] || '1fc790af87ad8f248dce004d8a52b4d3fe60997f'
 end

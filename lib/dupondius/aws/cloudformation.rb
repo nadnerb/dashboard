33b54e7c4993594f80ec56b8ee33dc6dc3abaed3
Stephen Bartlett <stephenb@rtlett.org>
2012-09-18 22:30:24 +1000

    Fix to the dashboard install

--- lib/dupondius/aws/cloudformation.rb
+++ lib/dupondius/aws/cloudformation.rb
@@ -149,7 +149,7 @@ module Dupondius; module Aws; module CloudFormation
 
       # inject the dashboard install script into the user-data
       user_data = template['Resources']['WebServer']['Properties']['UserData']['Fn::Base64']['Fn::Join'].last
-      user_data.insert((user_data.size) -4, "curl -L https://s3.amazonaws.com/dupondius/config/install-dashboard #{@tech_stack} #{@aws_region} | bash \n")
+      user_data.insert((user_data.size) -4, "curl -L https://s3.amazonaws.com/dupondius/config/install-dashboard | bash -s #{@tech_stack.parameterize} #{@aws_region}\n")
       JSON.pretty_generate(template)
     end
   end

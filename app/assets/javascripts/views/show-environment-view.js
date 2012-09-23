20a1bb7e0de22ad34d98995b0c7eb44003ff0ac6
azzamallow <azzamallow@gmail.com>
2012-09-23 11:20:34 +1000

    info button now working smoothly, just waiting for information to show

--- app/assets/javascripts/views/show-environment-view.js
+++ app/assets/javascripts/views/show-environment-view.js
@@ -16,23 +16,39 @@ define([
             this.model = new Stack();
 
             this.bindTo(this.model, 'change', function () {
-              this.model.fetch();
+                this.renderInformation();
+                this.fadeIn();
             });
         },
 
+        serialize: function () {
+            return {
+                model: this.model,
+                random: this.random
+            }
+        },
+
         fadeIn: function () {
-            this.$('#loading-fields-' + this.random).fadeOut();
-            this.$('.form-horizontal').css('visibility', 'visible').fadeIn();
+            this.$('#loading-' + this.random).fadeOut();
+            this.$('dl').css('visibility', 'visible').fadeIn();
         },
 
         fadeOut: function () {
             this.spinner();
-            this.$('#loading-fields-' + this.random).fadeIn();
-            this.$('.form-horizontal').css('visibility', 'visible').fadeOut();
+            this.$('#loading-' + this.random).fadeIn();
+        },
+
+        spinner: function () {
+            spinner('loading-' + this.random, 50, 45, 15, 3, '#888');
+        },
+
+        renderInformation: function () {
+            // this.$('.web-instances').html();
+            // this.$('.dns').html();
+            // ...
         },
 
         confirm: function () {
-
             return false;
         }
     });

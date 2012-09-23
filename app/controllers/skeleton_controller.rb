84b290c0aff01869a8354fa332dcb8cc0eaa51bc
Brendan Spinks <brendanspinks@gmail.com>
2012-09-23 11:40:29 +1000

    removing old unnused controller

--- app/controllers/skeleton_controller.rb
+++
@@ -1,9 +0,0 @@
-class SkeletonController < ApplicationController
-
-  wrap_parameters :format => [:json]
-
-  def create
-    Jobs::Skeleton.new(params[:name]).build
-    render :text => "Creating new project #{params[:name]}, please wait"
-  end
-end

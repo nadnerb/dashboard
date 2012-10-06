58d3493efe5d34550db2fd317f1daf34739cfb91
Ronald Holshausen <uglyog@gmail.com>
2012-10-06 12:18:02 +1000

    cleanup the grails skeleton generation

--- app/models/jobs/grails_skeleton.rb
+++ app/models/jobs/grails_skeleton.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class Jobs::GrailsSkeleton
 
   attr_reader :project, :resting_place
@@ -9,15 +11,17 @@ class Jobs::GrailsSkeleton
 
   def run
     p "building grails environment: #{resting_place} for project: #{project.name}"
-    p `cd #{resting_place};\
-    curl -u 'dupondius:4nalienatemybaby' -L -o grails.tar.gz https://api.github.com/repos/uglyog/dupondius-grails-skeleton/tarball;\
-    tar xfz grails.tar.gz;\
-    mv \`ls -d -- */\` #{project.name};\
-    cd #{project.name};\
-    sed 's/\\(app\\.name=\\).*/\\1showcase/' application.properties \#> application.properties.sed;\
-    mv application.properties.sed application.properties;\
-    git init;\
-    git add .;\
-    git commit --author='dupondi.us <commiter@dupondi.us>' -m 'Initial commit'`
+    FileUtils.cd(resting_place, :verbose => true) do
+      p `curl -u 'dupondius:4nalienatemybaby' -L -o grails.tar.gz https://api.github.com/repos/uglyog/dupondius-grails-skeleton/tarball;\
+      tar xfz grails.tar.gz;\
+      mv \`ls -d -- */\` #{project.name};`
+      FileUtils.cd(project.name, :verbose => true) do
+        `sed 's/\\(app\\.name=\\).*/\\1#{project.name}/ application.properties > application.properties.sed;\
+        mv application.properties.sed application.properties;\
+        git init;\
+        git add .;\
+        git commit --author='dupondi.us <commiter@dupondi.us>' -m 'Initial commit'`
+      end
+    end
   end
 end

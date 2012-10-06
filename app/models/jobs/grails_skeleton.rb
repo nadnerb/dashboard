4debad825ff7bf4ab695faeff3c587cd8eb5bc05
Ronald Holshausen <uglyog@gmail.com>
2012-10-06 12:29:38 +1000

    fixed missing qoute

--- app/models/jobs/grails_skeleton.rb
+++ app/models/jobs/grails_skeleton.rb
@@ -14,9 +14,9 @@ class Jobs::GrailsSkeleton
     FileUtils.cd(resting_place, :verbose => true) do
       p `curl -u 'dupondius:4nalienatemybaby' -L -o grails.tar.gz https://api.github.com/repos/uglyog/dupondius-grails-skeleton/tarball;\
       tar xfz grails.tar.gz;\
-      mv \`ls -d -- */\` #{project.name};`
+      mv \`ls -d -- */\` #{project.name}`
       FileUtils.cd(project.name, :verbose => true) do
-        `sed 's/\\(app\\.name=\\).*/\\1#{project.name}/ application.properties > application.properties.sed;\
+        `sed 's/\\(app\\.name=\\).*/\\1#{project.name}/' application.properties > application.properties.sed;\
         mv application.properties.sed application.properties;\
         git init;\
         git add .;\

803e2430912df6f1b4321c265f3e628ef7f68410
Stephen Bartlett <stephenb@rtlett.org>
2012-10-15 20:31:31 +1100

    Schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121002101152) do
+ActiveRecord::Schema.define(:version => 20121015003002) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -44,14 +44,17 @@ ActiveRecord::Schema.define(:version => 20121002101152) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.string   "token"
     t.string   "tech_stack"
     t.string   "region"
     t.string   "github_account"
     t.string   "github_project"
     t.boolean  "github_private"
+    t.string   "aws_access_key"
+    t.string   "aws_secret_access_key"
+    t.string   "aws_key_name"
   end
 
   create_table "server_configurations", :force => true do |t|
